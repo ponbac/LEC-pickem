535cabd691ce02eb0653ca241599b1b6ee37cccf
Pontus Backman <ponbac@student.chalmers.se>
2018-12-23 14:54:38 +0100

    started making schedule_parser into class

--- parser/schedule_parser.py
+++ parser/schedule_parser.py
@@ -1,27 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
-
-# Get page and pass it to bs4
-url = 'https://lol.gamepedia.com/EU_LCS/2018_Season/Summer_Season'
-page = requests.get(url)
-soup = BeautifulSoup(page.content, "html.parser")
-
-# Find relevant data
-schedule_table = soup.find('div', attrs={'class': 'tabs-dynamic'})
-weeks = schedule_table.find('div', attrs={'class': 'tabs-content'})
-week1 = weeks.find('div', attrs={'class': 'content1'}).find_all('tr')[2:]
+from model.match import Match
 
 
-# Print given week
-def print_week(week):
-    i = 0
-    for x in week:
-        teams = x.find_all('td')[1].find_all('div')[0].text
-        result = x.find_all('td')[2].text.replace('\n', '')
+class ScheduleParser:
+    def __init__(self, week_as_int):
+        # Get page and pass it to bs4
+        self.url = 'https://lol.gamepedia.com/EU_LCS/2018_Season/Summer_Season'
+        self.page = requests.get(self.url)
+        self.soup = BeautifulSoup(self.page.content, "html.parser")
 
-        i += 1
-        print(str(i) + ":" + teams + " Score: " + result)
+        # Find relevant data
+        self.schedule_table = self.soup.find('div', attrs={'class': 'tabs-dynamic'})
+        self.weeks = self.schedule_table.find('div', attrs={'class': 'tabs-content'})
+        self.week = self.weeks.find('div', attrs={'class': 'content' + str(week_as_int)}).find_all('tr')[2:]
+
+    # Print given week
+    def print_week(self):
+        i = 0
+        for x in self.week:
+            teams = x.find_all('td')[1].find_all('div')[0].text
+            result = x.find_all('td')[2].text.replace('\n', '')
+
+            i += 1
+            print(str(i) + ":" + teams + " Score: " + result)
+
+    # Get matches as match-objects
+    def get_matches(self):
+        i = 0
+        for x in self.week:
+            i += 1
+
+            teams = x.find_all('td')[1].find_all('div')[0].text
+            team1 = teams[:4].strip()
+            team2 = teams[-5:].strip()
+            result = x.find_all('td')[2].text.replace('\n', '')
+
+            match = Match(team1, team2, i)
+
+            if result[0] == '1':
+                print(team1 + " won!")
+            elif result[-1:] == '1':
+                print(team2 + ' won!')
 
 
-# Print week 1
-print_week(week1)
+
+
+sp = ScheduleParser(1)
+sp.get_matches()
